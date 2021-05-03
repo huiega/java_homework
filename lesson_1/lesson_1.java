91e6876a5c931a82fb5b905bdee5f20179d4f545
huiega <zaxarov11e@gmail.com>
2021-05-03 21:51:20 +0300

    1st commit

---
+++ lesson_1/lesson_1.java
@@ -0,0 +1,24 @@
+
+public class lesson_1 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+    public static void printThreeWords() {
+        int a = 45;
+        int b = 65;
+        int c = a+b;
+        if (c >= 0) {
+            System.out.print("Сумма положительная");
+        }
+        if (c <= 0) {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+}
