d8d5304491ed0afa89d21240f376ba4ead13f587
huiega <zaxarov11e@gmail.com>
2021-05-06 15:53:54 +0300

    1st lesson commit

--- lesson_1/lesson_1.java
+++ lesson_1/lesson_1.java
@@ -1,24 +1,50 @@
 
 public class lesson_1 {
     public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
 
-
+    public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
-
     }
-    public static void printThreeWords() {
+
+    public static void checkSumSign() {
         int a = 45;
-        int b = 65;
-        int c = a+b;
+        int b = -65;
+        int c = a + b;
         if (c >= 0) {
-            System.out.print("Сумма положительная");
+            System.out.println("Сумма положительная");
         }
         if (c <= 0) {
             System.out.println("Сумма отрицательная");
         }
+    }
+    public static void printColor() {
+        int value = 0;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }else if (value>0) {
+            System.out.println("Жёлтый");
+        }else if (value<=100) {
+            System.out.println("Жёлтый");
+        }else if (value<100) {
+            System.out.println("Зелёный");
+        }
 
     }
-
+    private static void compareNumbers() {
+        int a = 3;
+        int b = 2;
+        if (a>=b) {
+            System.out.println("a>=b");
+        }else if (a<b){
+            System.out.println("a<b");
+        }
+    }
 }
+
